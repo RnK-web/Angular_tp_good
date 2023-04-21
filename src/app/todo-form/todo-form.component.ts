9f3c41a640eeb649511fd6db22471a311114edbc
RnK-web <75388207+RnK-web@users.noreply.github.com>
2023-04-21 16:08:34 +0200

    2 files modified, 1 added

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styleUrls: ['./todo-form.component.css']
+  styleUrls: ['./todo-form.component.css'],
 })
 export class TodoFormComponent implements OnInit {
+  newTodo: String;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
